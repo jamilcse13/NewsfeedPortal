5ddeb9a336cadb31c4f61027aaec7c55caff7b76
jamil <jamil.cse13@gmail.com>
2022-06-22 02:36:26 +0600

    form validation for SignUp form

--- User/forms.py
+++ User/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core import validators
+from django.contrib.auth.models import User
 
 COUNTRY_CHOICES = (
     ('au', 'Australia'),
@@ -24,6 +26,14 @@ SOURCE_CHOICES = (
     ('reuters', 'Reuters')
 )
 
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    fname = forms.CharField(max_length=50)
+    lname = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=50)
+    pass1 = forms.CharField(widget = forms.PasswordInput, validators = [validators.MinLengthValidator(6)])
+    pass2 = forms.CharField(widget = forms.PasswordInput, validators = [validators.MinLengthValidator(6)])
 
 class SettingForm(forms.Form):
     country_field = forms.MultipleChoiceField(choices=COUNTRY_CHOICES)
--- User/views.py
+++ User/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from NewsApp import views
-from .forms import SettingForm
+from .forms import SettingForm, SignUpForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -13,33 +13,36 @@ def home(request):
 
 def signup(request):
     if request.method == "POST":
-        username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
-    
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
+        form = SignUpForm(request.POST)
 
-        myuser.save()
+        if form.is_valid():
+            username = request.POST['username']
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            email = request.POST['email']
+            pass1 = request.POST['pass1']
+            pass2 = request.POST['pass2']
+        
+            myuser = User.objects.create_user(username, email, pass1)
+            myuser.first_name = fname
+            myuser.last_name = lname
 
-        messages.success(request, "Your account has been created successfully!")
+            myuser.save()
 
-        return redirect('signin')
+            messages.success(request, "Your account has been created successfully!")
+
+            return redirect('signin')
     
     return render(request, "authentication/signup.html")
 
 def signin(request):
     if request.method == "POST":
         username = request.POST['username']
-        pass1 = request.POST['pass1']
+        password = request.POST['password']
 
-        user = authenticate(username=username, password=pass1)
+        user = authenticate(username=username, password=password)
 
-        if user is not None:
+        if user:
             login(request, user)
             fname = user.first_name
             return redirect(home)
